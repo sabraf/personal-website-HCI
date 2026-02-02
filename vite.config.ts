5b8619e983bbc92219413abff3c0bf47091fa7f1
S <sabarafaqat00@gmail.com>
2026-02-03 00:39:20 +0100

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    base: '/personal-website-HCI/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
